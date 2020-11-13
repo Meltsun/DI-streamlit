7feb7a59a78a6760e4881e9cd254e6b18cf54a63
WongXinyu <1327616444@qq.com>
2020-11-13 19:41:11 +0800

    ???

---
+++ Dataset_class.py
@@ -0,0 +1,103 @@
+"""
+数据集类
+
+"""
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+from sklearn.utils import shuffle
+
+class dataset:
+	nFeatures=40 #特征值数量。需要人工设置
+	target=None
+	data=None
+
+	#构造方法
+	def __init__(self,N=None)->None:
+		if(N!=None):
+			self.target=np.empty(N)
+			self.data=np.empty([N,self.nFeatures])
+		return None
+
+	#计算样本数
+	def count_sample(self):
+		return len(self.target)
+
+	#计算各个标签的样本数
+	def count_lable(self):
+		lables={}
+		for i in range(0,self.count_sample()):
+			thisTarget=self.target[i]
+			if thisTarget in lables.keys(): 
+				lables[thisTarget]+=1
+			else:
+				lables[thisTarget]=1
+		return lables
+
+	#从字符串列表生成数据集,待修改
+	def build_from_file(self,fileN):
+		file=pd.read_csv(fileN,index_col=None,header=0,encoding='gbk')
+		self.target=file['lable'].values
+		self.data=file.iloc[:,:-1].values
+		return file
+
+	#按照特定方式标准化
+	def zscore_standardize(self):
+		self.data=preprocessing.StandardScaler().fit_transform(self.data)
+		return None
+
+	def minmax_standardize(self):
+		self.data=preprocessing.MinMaxScaler().fit_transform(self.data)
+		return None
+
+	#切分数据,输入为前一返回数据集的样本数量
+	def split(self,N,randomState=None): 
+		N=round(N)
+		dataset1=dataset(self.count_sample()-N)
+		dataset2=dataset(N)
+		dataset1.data,dataset2.data,dataset1.target,dataset2.target=train_test_split(self.data,self.target, test_size=N,random_state=randomState,stratify=self.target)
+		return dataset2,dataset1
+
+	#根据条件生成数据集,输入为标签为所有标签为0的动作和所有为1的，返回生成的数据集
+	def conditional_extract(self,condition0,condition1): 
+		if(type(condition0)==type(0)):
+			condition0=[condition0]
+		if(type(condition1)==type(1)):
+			condition1=[condition1]
+		N=0
+		lables=self.count_lable()
+		for i in condition0+condition1:
+			N+=lables[i]
+		thisDataset=dataset(N)
+		N=0
+		for i in range(0,self.count_sample()):
+			thisLable=self.target[i]
+			if(thisLable in condition0):
+				thisDataset.data[N]=self.data[i]
+				thisDataset.target[N]=0
+				N+=1
+			elif(thisLable in condition1):
+				thisDataset.data[N]=self.data[i]
+				thisDataset.target[N]=1
+				N+=1
+		return thisDataset
+
+	#打乱
+	def shuffle(self,x=None):
+		self.data,self.target=shuffle(self.data,self.target,random_state=x)
+		return None
+
+#数据导入和预处理，返回一个数据集
+def preprocess(): 
+	totalData=dataset()
+	totalData.build_from_file("feature.csv")
+	print(f"\n各类样本数量:")
+	print(totalData.count_lable())
+	print('')
+	return totalData
+
+#打印实例的所有属性
+def obj_print(self): 
+	print('\n'.join(['%s:%s' % item for item in self.__dict__.items()]))
+	return None
--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import random
+import pickle
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
+from PIL import Image
 
 #import matplotlib
 #matplotlib.use('TkAgg')
@@ -12,12 +15,58 @@ mpl.rcParams['axes.unicode_minus'] = False
 import matplotlib.pyplot as plt
 
 @st.cache
+def load_classifier():
+	return pickle.load(open("svmClassifiers.pkl",'rb'))
+
+def load_classifier_2():
+	return pickle.load(open("knnClassifier.pkl",'rb'))
+
+@st.cache
+def load_standardizer():
+	return pickle.load(open("standardizer.pkl",'rb'))
+
+@st.cache
 def load_dataset():
 	return pd.read_csv("originSet.csv",header=None,index_col=None ).values
 
 @st.cache
 def load_features():
 	return pd.read_csv("feature.csv",index_col=None,header=0,encoding='gbk')
+
+@st.cache
+def load_testID():
+	return pd.read_csv("testID.csv",header=None,index_col=None ).values.ravel()
+
+def infer(thisFeature):
+	standardizer = load_standardizer()
+	thisFeature = standardizer.transform([thisFeature])
+	svmClassifier=load_classifier()
+	knnClassifier=load_classifier_2()
+	#得到验证样本的概率
+	knnResult=knnClassifier.predict_proba(thisFeature)
+	st.write("---")
+	st.write("两步分类-KNN粗分类；本样本为5种类型的概率分别为：")
+	knnResult
+	knnResult=knnResult[0]
+	#由knnResult分析得到 knn无法确定的样本的2个最大可能性
+	possibility=np.array([3,4])
+	maxID=knnResult.argmax()
+	maxP=knnResult[maxID]
+	if(maxP==1):
+		if(maxID!=4):
+			possibility[0]=maxID
+			possibility[1]=4
+	else:
+		possibility[0]=maxID
+		maxP=0
+		for j in range(0,5):
+			if(j!=maxID and knnResult[j]>maxP):
+				possibility[1]=j
+		possibility.sort()
+
+	x=svmClassifier[possibility[0]][possibility[1]].predict(thisFeature)
+	return possibility[round(x[0])]
+	
 
 #定义通用的变量
 dataSet=load_dataset()#所有数据
@@ -26,10 +75,9 @@ dsTypeN = ['无','车辆经过','浇水','敲击','攀爬']
 dsCruveN = ['时域','时域差分','小波包分解','经验模态分解']
 featureN=feature.columns#特征值名
 
-
-st.title('分布式光纤传感系统的扰动信号分析')
-st.title('【本程序仍在开发中，预计11月初完成】')
-agree = st.sidebar.radio("模式",['特征值分布分析','单个样本分析','分类器测试'])
+st.title('超长周界安防预警系统')
+st.title('测试与成果展示平台')
+agree = st.sidebar.radio("模式",["简介",'单个样本分析','特征值分布分析','扰动识别测试'])
 st.sidebar.write('---')
 
 if(agree=='单个样本分析'):
@@ -59,12 +107,7 @@ if(agree=='单个样本分析'):
 		st.dataframe(pd.DataFrame(dFeature[8:]).T)
 
 	elif(dsCruve=='时域差分'):
-		dataDiff=np.diff(data)
-		#图
-		figure1=plt.figure()
-		plt.plot([i for i in range(0,len(dataDiff))],dataDiff)
-		st.pyplot(figure1)
-
+		st.line_chart(np.diff(data))
 		dFeature=feature.iloc[dsType*150+dsNumber][:-1][15:30]
 		dFeature.index=[i.replace('差分-','') for i in dFeature.index]
 		st.dataframe(pd.DataFrame(dFeature[0:8]).T)
@@ -107,32 +150,105 @@ elif(agree=='特征值分布分析'):
 		plt.legend(loc = 'best')
 	st.pyplot(figure2)
 
-elif(agree=='分类器测试'):
-	st.write("功能测试中")
-	st.sidebar.write('超参数设置')
-	knnPointNum = st.sidebar.number_input('KNN-点数', min_value=1, max_value=20, value=5, step=1)
-	if(st.sidebar.button('启动分类器测试')):
-		dataID=[i for i in range(0,150)]
-		trainNum=120
-		trainID=np.empty([5,trainNum],dtype=int)
-		testID=np.empty([5,150-trainNum],dtype=int)
-		for i in range(0,5):
-			j=random.sample(dataID, trainNum)
-			trainID[i]=np.array(j)+i*150
-			testID[i]=np.array([i for i in dataID if i not in j])+i*150
-		trainFeature=feature.iloc[trainID.ravel()]
-		trainFeature
-		testFeature=feature.iloc[testID.ravel()]
+elif(agree=='扰动识别测试'):
+	st.write("---")
+	source=st.sidebar.radio("数据来源",['使用提供的数据','自行填入数据'])
+	if(source=='使用提供的数据'):
+		st.sidebar.write("---")
+		#获取侧栏的控制字
+		dsType = st.sidebar.selectbox('扰动类型' , dsTypeN)
+		target = dsType 
+		dsType = dsTypeN.index(dsType)
+		dsNumber = load_testID()[st.sidebar.slider('样本编号',1,1,30,1)-1] #整数
+		data=dataSet[dsType*3+dsNumber//50][dsNumber%50*22:dsNumber%50*22+33]
+		dFeature=feature.iloc[dsType*150+dsNumber,:-1]
+		st.write("样本数据如下：")
+		st.dataframe(pd.DataFrame(data).T)
+		st.write("样本的特征值如下：")
+		st.dataframe(pd.DataFrame(dFeature).T)
 
+		dFeature=dFeature.values
+		
+	else:
+		"功能仍在完善中，尚未启用"
+		text=st.text_area('请在此输入33个数据,用逗号或回车分割', value='', key=None)
+		if(st.button('填入数据')):
+			st.write("= = 还没做完")
+		dFeature=[0 for i in range(0,40)]
 
+	
+	if(source=='使用提供的数据'):
+		result=infer(dFeature)
+		st.write(f"两步分类-SVM分类：本样本对应的扰动类型为 {dsTypeN[result]}扰动 。")
+		st.write(f"本样本的实际扰动类型为 {target}扰动。")
+		if(dsType==result):
+			st.write("推理正确 。")
+		else:
+			st.write(f"推理错误 。")
+
+	st.sidebar.write("---")
+	dsCruve = st.sidebar.selectbox('查看信号波形和相关特征值' , ["(关闭)"]+dsCruveN)#字符串
+	st.write("---")
+	if(dsCruve=='时域'):
+		#ser=pd.Series(data.tolist())
+		#ser.plot()
+		st.line_chart(data)
+		#st.pyplot()
+		#图
+		dFeature=feature.iloc[dsType*150+dsNumber][:-1][0:15]
+		st.dataframe(pd.DataFrame(dFeature[0:8]).T)
+		st.dataframe(pd.DataFrame(dFeature[8:]).T)
+
+	elif(dsCruve=='时域差分'):
+		st.line_chart(np.diff(data))
+		dFeature=feature.iloc[dsType*150+dsNumber][:-1][15:30]
+		dFeature.index=[i.replace('差分-','') for i in dFeature.index]
+		st.dataframe(pd.DataFrame(dFeature[0:8]).T)
+		st.dataframe(pd.DataFrame(dFeature[8:]).T)
+
+	elif(dsCruve=='小波包分解'):
+		dFeature=feature.iloc[dsType*150+dsNumber][:-1][30:40]
+		st.dataframe(pd.DataFrame(dFeature[0:5]).T)
+		st.dataframe(pd.DataFrame(dFeature[5:]).T)
+		dsCruveWP = st.sidebar.selectbox('选择小波图像' , ['最低频','低频','高频','最高频'])
+		#根据dsCruveWP的值（字符串）画不同的图
+	
+elif(agree=="简介"):
+	'---'
+	st.header("项目概述")
+	"""
+	"超长周界安防预警系统"是一个用于国境线、输油管线、机场或大型基地周边等场景的长周界安防预警系统。
+	本系统将与新型传感器紧密结合，使用多种机器学习算法和信号处理手段，最终实现对入侵行为的自动识别与报警
+	。本项目即是为上述系统设计算法并开发软件。
+	"""
+	image = Image.open('image1.png')
+	st.image(image, caption='分布式光纤扰动传感系统',width=600)
+
+	st.header("项目进展")
+	"""
+	目前信号处理和机器学习的核心算法已经基本设计完成。针对8微秒的信号样本，我们提取了总共40维特征值，
+	使用KNN+SVM结合的方法对其进行模式识别，正确率达到90%。算法和代码层面的优化仍在持续进行中。
+	"""
+	'客户端软件(即本项目的最终成果）将在核心算法优化完成后开始开发。在核心算法确定且机器学习分类器训练完成的情况下，预计该软件的开发周期不会太长。'
+	
+	image = Image.open('image2.png')
+	st.image(image, caption='核心算法的测试结果',width=400)
+
+	st.header("关于本应用")
+	"""本应用用于展示项目的中期成果，以及以可视化的方式进行数据分析与相关测试。请在侧栏从上至下依次设置参数。功能介绍如下：
+	\n单个样本分析：查看一个信号样本的波形图和相关的特征值查看样本。
+	\n特征值分布分析：选定几种信号，查看这些信号的2种特征值的二维分布。这能直观的反映出特征值对不同信号的区分作用。
+	\n扰动识别测试：手动输入一组样本或者选择一组给定的样本，并使用机器学习分类器对样本的扰动类型进行识别。
+	""" 
 
 else:
 	st.write("没做完 = =")
 
-st.write('---')
-st.write('请在侧栏从上至下依次设置参数')
-st.text('点击左上角 > 打开侧栏')
-st.text('发生错误时请尝试点击右上角 ☰ → Clear cache 清除缓存')
-st.text('本应用基于python-streamlit ， 右上角有小人在动即正在加载，请稍等')
-st.text('数据更新时间：'+'2020年10月19日00:36:39')
-st.text('by BJTU_WXY')
+if(agree!="简介"):
+	st.write('---')
+	st.write('请在侧栏从上至下依次设置参数')
+	st.text('点击左上角 > 打开侧栏')
+	st.text('发生错误时请尝试点击右上角 ☰ → Clear cache 清除缓存')
+	st.text('本应用基于python-streamlit ， 右上角有小人在动即正在加载，请稍等')
+	st.text('数据更新时间：'+'2020年11月13日18:38:19')
+	st.text('by BJTU_WXY')
